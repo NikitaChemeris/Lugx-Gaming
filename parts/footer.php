f2cab3303248dca8c6ea8b5933b797f50be888d0
Nikita_IT_2004 <112660966+NikitaChemeris@users.noreply.github.com>
2024-04-04 01:41:37 +0200

    Added folder "parts"
    Also I connected project to XAMPP and footer.php, header.php were added.

---
+++ parts/footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <footer>
+        <div class="container">
+        <div class="col-lg-12">
+            <p>Copyright © 2048 LUGX Gaming Company. All rights reserved. &nbsp;&nbsp; <a rel="nofollow" href="https://templatemo.com" target="_blank">Design: TemplateMo</a></p>
+        </div>
+        </div>
+    </footer>
+</body>
+</html>
