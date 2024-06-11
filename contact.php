2e386667d55c41b37ce24e27543fafa0dbc352a5
Nikita <nikitacemeris9@gmail.com>
2024-06-11 11:03:27 +0200

    Completed ContactUS

--- contact.php
+++ contact.php
@@ -1,115 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <?php require "parts/head.php" ?>
+<?php require "parts/head.php" ?>
 
 <body>
 
-  <!-- ***** Preloader Start ***** -->
-  <div id="js-preloader" class="js-preloader">
+<!-- ***** Preloader Start ***** -->
+<div id="js-preloader" class="js-preloader">
     <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  </div>
-  <!-- ***** Preloader End ***** -->
-
-  <?php require "parts/header.php" ?>
-
-  <!-- ***** Header Area End ***** -->
-
-  <div class="page-heading header-text">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <h3>Contact Us</h3>
-          <span class="breadcrumb"><a href="#">Home</a>  >  Contact Us</span>
+        <span class="dot"></span>
+        <div class="dots">
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
-      </div>
     </div>
-  </div>
+</div>
+<!-- ***** Preloader End ***** -->
 
-  <div class="contact-page section">
+<?php require "parts/header.php" ?>
+
+<!-- ***** Header Area End ***** -->
+
+<div class="page-heading header-text">
     <div class="container">
-      <div class="row">
-        <div class="col-lg-6 align-self-center">
-          <div class="left-text">
-            <div class="section-heading">
-              <h6>Contact Us</h6>
+        <div class="row">
+            <div class="col-lg-12">
+                <h3>Contact Us</h3>
+                <span class="breadcrumb"><a href="#">Home</a>  >  Contact Us</span>
             </div>
-            <p>LUGX Gaming Template is based on the latest Bootstrap 5 CSS framework. This template is provided by TemplateMo and it is suitable for your gaming shop ecommerce websites. Feel free to use this for any purpose. Thank you.</p>
-            <ul>
-              <li><span>Address</span>Trieda Andreja Hlinku 1, 949 74 Nitra-Chrenová, Slovakia</li>
-              <li><span>Phone</span>+421 950 726 722</li>
-              <li><span>Email</span><a href="mailto:nikitacemeris9@gmail.com">nikitacemeris9@gmail.com</a></li>
-            </ul>
-          </div>
         </div>
-        <div class="col-lg-6">
-          <div class="right-content">
-            <div class="row">
-              <div class="col-lg-12">
-                <div id="map">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21740552.93289926!2d-20.938169500000022!3d48.30523449999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476b3ee43b2f6763%3A0x75a567f979f5bed3!2sConstantine%20the%20Philosopher%20University!5e0!3m2!1sen!2sua!4v1712777779053!5m2!1sen!2sua" width="100%" height="325px" frameborder="0" style="border:0; border-radius: 23px;" allowfullscreen=""></iframe>
+    </div>
+</div>
+
+<div class="contact-page section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 align-self-center">
+                <div class="left-text">
+                    <div class="section-heading">
+                        <h6>Contact Us</h6>
+                    </div>
+                    <p>LUGX Gaming Template is based on the latest Bootstrap 5 CSS framework. This template is provided by TemplateMo and it is suitable for your gaming shop ecommerce websites. Feel free to use this for any purpose. Thank you.</p>
+                    <ul>
+                        <li><span>Address</span>Trieda Andreja Hlinku 1, 949 74 Nitra-Chrenová, Slovakia</li>
+                        <li><span>Phone</span>+421 950 726 722</li>
+                        <li><span>Email</span><a href="mailto:nikitacemeris9@gmail.com">nikitacemeris9@gmail.com</a></li>
+                    </ul>
                 </div>
-              </div>
-              <div class="col-lg-12">
-                <form id="contact-form" action="" method="post">
-                  <div class="row">
-                    <div class="col-lg-6">
-                      <fieldset>
-                        <input type="name" name="name" id="name" placeholder="Your Name..." autocomplete="on" required>
-                      </fieldset>
-                    </div>
-                    <div class="col-lg-6">
-                      <fieldset>
-                        <input type="surname" name="surname" id="surname" placeholder="Your Surname..." autocomplete="on" required>
-                      </fieldset>
-                    </div>
-                    <div class="col-lg-6">
-                      <fieldset>
-                        <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your E-mail..." required="">
-                      </fieldset>
-                    </div>
-                    <div class="col-lg-6">
-                      <fieldset>
-                        <input type="subject" name="subject" id="subject" placeholder="Subject..." autocomplete="on" >
-                      </fieldset>
-                    </div>
-                    <div class="col-lg-12">
-                      <fieldset>
-                        <textarea name="message" id="message" placeholder="Your Message"></textarea>
-                      </fieldset>
-                    </div>
-                    <div class="col-lg-12">
-                      <fieldset>
-                        <button type="submit" id="form-submit" class="orange-button">Send Message Now</button>
-                      </fieldset>
-                    </div>
-                  </div>
-                </form>
-              </div>
             </div>
-          </div>
+            <div class="col-lg-6">
+                <div class="right-content">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div id="map">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21740552.93289926!2d-20.938169500000022!3d48.30523449999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476b3ee43b2f6763%3A0x75a567f979f5bed3!2sConstantine%20the%20Philosopher%20University!5e0!3m2!1sen!2sua!4v1712777779053!5m2!1sen!2sua" width="100%" height="325px" frameborder="0" style="border:0; border-radius: 23px;" allowfullscreen=""></iframe>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <form id="contact-form" action="functions/submit_contact.php" method="post">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <fieldset>
+                                            <input type="name" name="name" id="name" placeholder="Your Name..." autocomplete="on" required>
+                                        </fieldset>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <fieldset>
+                                            <input type="surname" name="surname" id="surname" placeholder="Your Surname..." autocomplete="on" required>
+                                        </fieldset>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <fieldset>
+                                            <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your E-mail..." required="">
+                                        </fieldset>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <fieldset>
+                                            <input type="subject" name="subject" id="subject" placeholder="Subject..." autocomplete="on" >
+                                        </fieldset>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <fieldset>
+                                            <textarea name="message" id="message" placeholder="Your Message"></textarea>
+                                        </fieldset>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <fieldset>
+                                            <button type="submit" id="form-submit" class="orange-button">Send Message Now</button>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 
-  <?php require "parts/footer.php"?>
+<?php require "parts/footer.php"?>
 
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/js/isotope.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-  <script src="assets/js/counter.js"></script>
-  <script src="assets/js/custom.js"></script>
+<!-- Scripts -->
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/isotope.min.js"></script>
+<script src="assets/js/owl-carousel.js"></script>
+<script src="assets/js/counter.js"></script>
+<script src="assets/js/custom.js"></script>
 
-  </body>
+</body>
 </html>
