d50a06d1c59ae547c900f5922e36cf4a42975081
Nikita <nikitacemeris9@gmail.com>
2024-06-11 03:46:48 +0200

    Merge branch CRUD_for_games

--- product-details.php
+++ product-details.php
@@ -48,14 +48,8 @@
           <span class="price"><em>$28</em> $22</span>
           <p>LUGX Gaming Template is based on the latest Bootstrap 5 CSS framework. This template is provided by TemplateMo and it is suitable for your gaming shop ecommerce websites. Feel free to use this for any purpose. Thank you.</p>
           <form id="qty" action="#">
-            <input type="qty" class="form-control" id="1" aria-describedby="quantity" placeholder="1">
             <button type="submit"><i class="fa fa-shopping-bag"></i> ADD TO CART</button>
           </form>
-          <ul>
-            <li><span>Game ID:</span> COD MMII</li>
-            <li><span>Genre:</span> <a href="#">Action</a>, <a href="#">Team</a>, <a href="#">Single</a></li>
-            <li><span>Multi-tags:</span> <a href="#">War</a>, <a href="#">Battle</a>, <a href="#">Royal</a></li>
-          </ul>
         </div>
         <div class="col-lg-12">
           <div class="sep"></div>
@@ -63,7 +57,7 @@
       </div>
     </div>
   </div>
-  
+
 
   <?php require "parts/footer.php"?>
 
